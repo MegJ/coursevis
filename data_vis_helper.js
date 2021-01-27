0593f7d34f1c8a3e86bc580e26f25541605ada65
Lesley Huang <ningning621@gmail.com>
2021-01-26 23:55:07 -0500

    reorg code for demo bar graph

---
+++ data_vis_helper.js
@@ -0,0 +1,118 @@
+/** FUNCTIONS FOR GENERATING A BAR GRAPH **/
+function getBarX(data, svgWidth, xd) {
+  return d3.scaleBand()
+    .domain(data.map(function(d) {return d[xd];}))
+    .range([padding*5, svgWidth-(padding*5)])
+    .paddingInner(0.4);
+}
+
+function getBarY(data, svgHeight) {
+  return d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) {return +d.value;})])
+    .range([svgHeight-padding*3, padding*3]);
+}
+
+function drawBars(svg, x, y, data, maxBarHeight, barWidth, color) {
+  // create initial bars
+  svg.selectAll(".bars")
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("x", function(d) {return x(d.key);})
+      .attr("y", function(d) {return y(0);})
+      .attr("fill", function(d) {return color;})
+      .attr("height", function(d) {return maxBarHeight - y(0);})
+      .attr("width", x.bandwidth());
+
+  // animate the bars
+  svg.selectAll("rect")
+    .transition()
+    .duration(800)
+    .attr("y", function(d) {return y(d.value);})
+    .attr("height", function(d) {return maxBarHeight - y(d.value);})
+    .delay(function(d, i) {return i*100;});
+}
+
+/* adds text for value on top of respective bar for a bar graph */
+function drawTotalText(barSvg, x, y, data, barWidth, gutter) {
+  // create total text above bar
+    barSvg.selectAll(".total_text")
+      .data(data)
+    .enter()
+      .append("text")
+        .attr("class", "total_text")
+        .text(function(d) {return d.value})
+        .attr("x", function(d) {return x(d.key) + x.bandwidth()/2;})
+      .attr("y", function(d) {return y(d.value) - gutter;})
+      .style("text-anchor", "middle")
+      .style("fill", darkTextColor)
+      .style("font-weight", "bold")
+      .style("font-family", "Inconsolata")
+          .style("font-size", "12px")
+          .style("opacity", 0);
+
+    // animate text
+    barSvg.selectAll(".total_text")
+      .transition()
+      .duration(900)
+      .style("opacity", 1)
+      .delay(function(d, i) {return i*100;});
+}
+
+/* used for wrapping text in bar graph axes labels */
+// code adapted from https://bl.ocks.org/mbostock/7555321
+function wrap(text, width) {
+  text.each(function() {
+  var text = d3.select(this),
+      words = text.text().split(/\s+/).reverse(),
+      word,
+      line = [],
+      lineNumber = 0,
+      lineHeight = 1.1, // ems
+      y = text.attr("y"),
+      dy = parseFloat(text.attr("dy")),
+      tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+  while (word = words.pop()) {
+    line.push(word);
+    tspan.text(line.join(" "));
+    if (tspan.node().getComputedTextLength() > width) {
+      line.pop();
+      tspan.text(line.join(" "));
+      line = [word];
+      tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+    }
+  }
+  });
+}
+
+/* FUNCTIONS FOR HANDLING BAR GRAPH ANIMATIONS */
+
+/* bar graph axes will need to be redrawn when the represented data changes */
+function redrawXAxis(x, maxBarHeight, slantText = false) {
+  d3.selectAll(".x_axis")
+    .transition()
+    .duration(0)
+    .call(d3.axisBottom(x))
+    .attr("transform", "translate(" + 0 + ", " + (maxBarHeight) + ")");
+
+  d3.select(".domain").remove();
+    
+  if (slantText) {
+    d3.selectAll(".x_axis .tick text")
+          .attr("transform", "rotate(-25)")
+          .style("text-anchor", "end");
+  }
+}
+
+/* handles animation for bar graph when represented data changes */
+function handleBarLegendChange(d, circle_id) {
+  d3.selectAll(circle_id)
+    .transition()
+    .duration(500)
+    .style("fill", "#f8f8f8");
+
+  d3.select("#key_" + d)
+    .transition()
+    .duration(500)
+    .style("fill", darkTextColor);
+}
---
+++ vis/demographic_vis.js
@@ -0,0 +1,132 @@
+function drawBarChart(svgClass) {
+  let maxBarHeight = firstSvgHeight - padding*3;
+  let barWidth = padding*2;
+
+  let genderData = createGenderData();
+  let yearData = createYearData();
+  let ethnicityData = createEthnicityData();
+  let activitiesData = createActivitiesData();
+  let collegeData = createCollegeData();
+
+  let barSvg = d3.select(svgClass);
+  
+  // addTotalSignupText(barSvg)
+
+  let x = getBarX(genderData, firstSvgWidth, "key");
+  let y = getBarY(genderData, firstSvgHeight);
+
+  drawBars(barSvg, x, y, genderData, maxBarHeight, barWidth, "#A7333F");
+
+  // add x axis
+  barSvg.append("g")
+    .attr("class", "x_axis")
+    .attr("transform", "translate(" + 0 + ", " + (maxBarHeight) + ")")
+    .call(d3.axisBottom(x))
+    .style("font-family", "Inconsolata")
+        .style("font-size", "12px")
+        .select(".domain").remove();
+
+    drawTotalText(barSvg, x, y, genderData, barWidth, 5);
+  
+    // key for bar chart
+     let barLegend = ["gender", "year", "ethnicity", "college", "activities"];
+
+     // create legend
+     d3.select(svgClass).append("g")
+     .selectAll(".key")
+       .data(barLegend)
+       .enter()
+       .append("circle")
+       .attr("class", "legend_circles")
+       .attr('id', function(d, i) { return "key_" + d;})
+      .attr('cx', function(d, i) { return firstSvgWidth-padding*4;})
+      .attr('cy', function(d, i) { return 160 + i*30;})
+      .attr('r', 8)
+      .style('fill', '#f8f8f8')
+      .style('stroke', darkTextColor)
+      .style('stroke-width', 2)
+      .on('click', function(d, i) {
+        handleBarLegendChange(d, ".legend_circles");
+        barSvg.selectAll("rect").remove();
+        barSvg.selectAll(".total_text").remove();
+        
+        if (d == "year") {
+          x = getBarX(yearData, firstSvgWidth, "key");
+          y = getBarY(yearData, firstSvgHeight);
+
+          drawBars(barSvg, x, y, yearData, maxBarHeight, barWidth, "#A7333F");
+          drawTotalText(barSvg, x, y, yearData, barWidth, 5);
+          redrawXAxis(x, maxBarHeight);
+
+        }
+        else if (d == "gender") {
+          x = getBarX(genderData, firstSvgWidth, "key");
+          y = getBarY(genderData, firstSvgHeight);
+
+          drawBars(barSvg, x, y, genderData, maxBarHeight, barWidth, "#A7333F");
+          drawTotalText(barSvg, x, y, genderData, barWidth, 5);
+          redrawXAxis(x, maxBarHeight);
+        } else if (d == "ethnicity") {
+          x = getBarX(ethnicityData, firstSvgWidth, "key");
+          y = getBarY(ethnicityData, firstSvgHeight);
+
+          drawBars(barSvg, x, y, ethnicityData, maxBarHeight, barWidth, maroonColor);
+          drawTotalText(barSvg, x, y, ethnicityData, barWidth, 5);
+          redrawXAxis(x, maxBarHeight);
+
+        } else if (d == "college") {
+          x = getBarX(collegeData, firstSvgWidth, "key");
+          y = getBarY(collegeData, firstSvgHeight);
+
+          drawBars(barSvg, x, y, collegeData, maxBarHeight, barWidth, maroonColor);
+          drawTotalText(barSvg, x, y, collegeData, barWidth, 5);
+          redrawXAxis(x, maxBarHeight);
+
+        } else if (d == "activities") {
+          x = getBarX(activitiesData, firstSvgWidth, "key");
+          y = getBarY(activitiesData, firstSvgHeight);
+
+          drawBars(barSvg, x, y, activitiesData, maxBarHeight, barWidth, maroonColor);
+          drawTotalText(barSvg, x, y, activitiesData, barWidth, 5);
+          redrawXAxis(x, maxBarHeight, true);
+
+        }
+      });
+
+    handleBarLegendChange("gender", ".legend_circles");
+
+    // create legend labels
+    d3.select(svgClass).append("g")
+    .selectAll('.key_labels')
+    .data(barLegend)
+      .enter()
+      .append('text')
+      .attr('x', function(d, i) { return firstSvgWidth-padding*4 + 20;})
+      .attr('y', function(d, i) { return 164 + i*30;})
+      .text(function(d) {return d;})
+      .style('fill', darkTextColor)
+      .style("font-weight", "bold")
+      .style("font-family", "Inconsolata")
+       .style("font-size", "12px");
+}
+
+function addTotalSignupText(barSvg) {
+  barSvg.append("text")
+    .attr("x", "50%")
+    .attr("y", padding*2)
+    .text("In 2020, we have " + getTotalPeople() + " people signed up to meet their perfect match!")
+    .style("text-anchor", "middle")
+    .style("fill", "#f8f8f8")
+    .style("font-family", "Montserrat")
+    .style("font-size", "20px")
+    .style("font-weight", "bold");
+  barSvg.append("text")
+    .attr("x", "50%")
+    .attr("y", padding*3.5)
+    .text("Let's take a look at their breakdown:")
+    .style("text-anchor", "middle")
+    .style("fill", "#f8f8f8")
+    .style("font-family", "Montserrat")
+    .style("font-size", "20px")
+    .style("font-weight", "bold");
+}
